9a9f72b459f1bfba9f3164411794083577a5fd48
Soham-Kumar <69777935+Soham-Kumar@users.noreply.github.com>
2023-06-14 17:42:57 +0530

    Create py_game.py
    The game built in PyGame

---
+++ py_game.py
@@ -0,0 +1,106 @@
+import sys
+import pygame
+
+pygame.init()
+
+# set up the window
+size = (600, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("RL Task for Sally")
+
+# set up the board
+board = pygame.Surface((600, 600))
+board.fill((255, 206, 158))
+
+# draw the board
+for x in range(3):
+    for y in range(3):
+        if((x+y) % 2 == 0):
+            pygame.draw.rect(board, (150, 150, 0), (x*200, y*200, 200, 200))
+        else:
+            pygame.draw.rect(board, (210, 180, 140), (x*200, y*200, 200, 200))
+
+        
+# add the board to the screen
+screen.blit(board, (0, 0))
+
+pygame.display.flip()
+
+
+
+#  Piece class whose instances will represent each piece on the board.
+class Piece:
+    def __init__(self, color, x, y):
+        self.color = color
+        self.x = x
+        self.y = y
+
+    def draw(self, surface):
+        try:
+            img = pygame.image.load(f"c:/VS Code Programs/ML/geinforecement_learning/{self.color}.png")
+        except pygame.error as e:
+            print("Error loading image:", e)
+        img_small = pygame.transform.scale(img, (180,180))
+        surface.blit(img_small, (self.x*200+10, self.y*200+10))
+
+
+# set up the pieces
+pieces = []
+for i in range(3):
+    pieces.append(Piece("black", i, 0))
+    pieces.append(Piece("white", i, 2))
+print(pieces)
+
+# draw the pieces
+for piece in pieces:
+    piece.draw(board)
+
+
+
+
+# main loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # get the position of the click
+            pos = pygame.mouse.get_pos()
+
+            # convert the position to board coordinates
+            x = (pos[0]) // 200
+            y = (pos[1]) // 200
+
+            # find the piece at the clicked position
+            for piece in pieces:
+                if piece.x == x and piece.y == y:
+                    # move the piece
+                    pos = pygame.mouse.get_pos()
+                    x = (pos[0]) // 200
+                    y = (pos[1]) // 200
+                    piece.x = x
+                    piece.y = y
+            
+            print(pieces)
+
+    # redraw the board and pieces
+    board.fill((255, 206, 158))
+    for x in range(3):
+        for y in range(3):
+            if((x+y) % 2 == 0):
+                pygame.draw.rect(board, (0, 150, 0), (x*200, y*200, 200, 200))
+            else:
+                pygame.draw.rect(board, (210, 180, 140), (x*200, y*200, 200, 200))
+
+    for piece in pieces:
+        piece.draw(board)
+
+
+    # add the board to the screen
+    screen.blit(board, (0, 0))
+
+    # update the display
+    pygame.display.update()
